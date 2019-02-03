29255bc0ad754273fe0dc246746ec217343f743b
joepietrzyk <joepietrzyk@gmail.com>
2019-02-02 19:08:19 -0500

    Restructure PingBlacklist
    Restructured to look more object-oriented

--- PingBlacklist.plugin.js
+++ PingBlacklist.plugin.js
@@ -1,53 +1,48 @@
 //META{"name":"PingBlacklist", "website": "https://github.com/joepietrzyk/BetterDiscordPlugins", "source": "https://github.com/joepietrzyk/BetterDiscordPlugins/blob/master/PingBlacklist.plugin.js"}*//
 
-var PingBlacklist = function() {};
+class PingBlacklist {
+    getName() { return "Ping Blacklist";    }
+    getDescription() {  return "Ignores pings from all users on your blacklist.";    }
+    getVersion() {  return "0.0.2";     }
+    getAuthor () {  return "Joseph Pietrzyk";    }
+    
+    start () 
+    {
+        // bdPluginStorage.set("PingBlacklist", "blacklist", {"users":[]});
+        this.blacklist = bdPluginStorage.get("PingBlacklist", "blacklist");
+    }
 
-PingBlacklist.prototype.checkBlacklist = function () {
-	var self = this;
+    checkBlacklist()
+    {
+        // TODO
+    }
 
+    getSettingsPanel() 
+    {
+        /* var panel = $("<form>").addClass("form").css("width", "100%");
+        this.generateSettings(panel);
+        alert(panel[0]); */
+        return "<h2>Coming soon!</h2>";
+    }
+
+    generateSettings(panel) 
+    {
+        new PluginSettings.ControlGroup("Settings", () => {
+            this.saveSettings();
+        }, {
+            shown: true
+        }).appendTo(panel).append(
+        new PluginSettings.Checkbox("Test", "This is a test.",
+         true, (checked) => {
+                if (checked) {
+                    alert("checked");
+                }
+           }));
+    }
+
+    onMessage() {}
+    onSwitch() {}
+    load() {}
+    unload() {}
+    stop() {}
 }
-
-PingBlacklist.prototype.getSettingsPanel = function () {
-    /* var panel = $("<form>").addClass("form").css("width", "100%");
-    this.generateSettings(panel);
-    alert(panel[0]); */
-    return "<h2>Coming soon!</h2>";
-};
-
-PingBlacklist.prototype.generateSettings = function(panel) {
-    new PluginSettings.ControlGroup("Settings", () => {
-        this.saveSettings();
-    }, {
-        shown: true
-    }).appendTo(panel).append(
-    new PluginSettings.Checkbox("Test", "This is a test.",
-        true, (checked) => {
-            if (checked) {
-                alert("checked");
-            }
-        }));
-};
-
-PingBlacklist.prototype.onMessage = function () {
-};
-PingBlacklist.prototype.onSwitch = function () {};
-PingBlacklist.prototype.start = function () {
-    // bdPluginStorage.set("PingBlacklist", "blacklist", {"users":[]});
-    this.blacklist = bdPluginStorage.get("PingBlacklist", "blacklist");
-};
-PingBlacklist.prototype.load = function () {};
-PingBlacklist.prototype.unload = function () {};
-PingBlacklist.prototype.stop = function () {};
-
-PingBlacklist.prototype.getName = function () {
-    return "Ping Blacklist";
-};
-PingBlacklist.prototype.getDescription = function () {
-    return "Ignores pings from all users on your blacklist.";
-};
-PingBlacklist.prototype.getVersion = function () {
-    return "0.0.1";
-};
-PingBlacklist.prototype.getAuthor = function () {
-    return "Joseph Pietrzyk";
-};
