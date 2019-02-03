a58b93e9875b8d5ce80b2b966ce4c2dbce3a8740
joepietrzyk <joepietrzyk@gmail.com>
2019-02-02 22:40:45 -0500

    Added inheritance
    PingBlacklist now extends ZeresPluginUtilities plugins

--- PingBlacklist.plugin.js
+++ PingBlacklist.plugin.js
@@ -9,26 +9,46 @@ var PingBlacklist = (() => {
         "description": "Ignores pings from users on your blacklist.", 
         "github":"https://github.com/joepietrzyk/BetterDiscordPlugins", 
         "github_raw": "https://github.com/joepietrzyk/BetterDiscordPlugins/blob/master/PingBlacklist.plugin.js"}};
-    return !global.ZeresPluginLibrary ? class {
+    // Ensure they have the correct dependcies
+    return !global.ZeresPluginLibrary ?
+    // send them a dummy plugin if they don't that directs them to install
+    class {
         getName() { return config.info.name;    }
         getDescription() {  return config.info.description;    }
         getVersion() {  return config.info.version;     }
-        getAuthor () {  config.info.authors.map(a => a.name).join(", ");    }
-        load() {  window.BdApi.alert("Library Missing", "You dun goofed");}
+        getAuthor() {  config.info.authors.map(a => a.name).join(", ");    }
+        load() {  window.BdApi.alert("Library Missing",`The library plugin needed for ${config.info.name} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);}
         start() {} 
         stop() {}
     } : (([Plugin, Api]) => {
         const plugin = (Plugin, Api) => {
-            return class PingBlacklist {
-                 getName() { return "Ping Blacklist";    }
-        getDescription() {  return "Ignores pings from users on your blacklist.";    }
-        getVersion() {  return "0.0.2";     }
-        getAuthor () {  return "Joseph Pietrzyk";    }
-        load() {  window.BdApi.alert("It's on!","You did alright!");}
-        start() {} 
-        stop() {}
+            return class PingBlacklist extends Plugin {
+                 constructor()
+                 {
+                    super();
+                    // TODO:
+                 }
+
+                 /* Executes when the plugin is started up
+                  *
+                  */
+                 onStart()
+                 {
+                    // this.settings = PluginUtilities.loadSettings();
+                 }
+
+                 onStop() {}
+
+                 getSettingsPanel()
+                 {
+                    var html = `
+                        <h2>PingBlacklist</h2>
+                        <p>Coming soon!</p>
+                    `;
+                    return html;
+                 }
             };
         };
         return plugin(Plugin, Api);
-    })(["yo", "lo"]);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
 })();
